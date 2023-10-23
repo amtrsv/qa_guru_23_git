f6f56de315a2179f3a754a07c0338d33bbc22983
Andrey Matrosov <99250363+amtrsv@users.noreply.github.com>
2023-10-24 02:39:20 +0300

    Update Main.java

--- src/main/java/ru/sberbank/Main.java
+++ src/main/java/ru/sberbank/Main.java
@@ -1,7 +1,7 @@
 package ru.sberbank;
 
 public class Main {
-    privte static String msg = "Hellow world!!!!!!!";
+    private static String msg = "Hellow world!!!!!!!";
     public static void main(String[] args) {
         System.out.println(msg);
     }
